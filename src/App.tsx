4dcaef67e89c41ede5ee3c5f6b35f0d1c1f0a7b5
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2025-01-11 08:52:23 -0300

    feat: project initial settings

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyles } from "./styles/GlobalStyles";
+
+export function App() {
+
+  return (
+    <>
+      <GlobalStyles />
+      <h1>
+        Hello!
+      </h1>
+    </>
+  )
+}
